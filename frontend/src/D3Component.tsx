5f14170e56dd0952dce00c4d34b073c6ac7aae74
Fanilo Andrianasolo <andfanilo@gmail.com>
2020-06-01 10:37:40 +0200

    :lipstick: Clean and document code

--- frontend/src/D3Component.tsx
+++ frontend/src/D3Component.tsx
@@ -4,6 +4,9 @@ import * as d3 from "d3";
 
 import styles from './D3Component.module.css'
 
+/**
+ * Margin between svg container and D3 graph bounds
+ */
 interface d3Margin {
     top: number
     bottom: number
@@ -11,6 +14,9 @@ interface d3Margin {
     right: number
 }
 
+/**
+ * Arguments Streamlit receives from the Python side
+ */
 interface PythonArgs {
     svgWidth: number
     svgHeight: number
@@ -18,6 +24,9 @@ interface PythonArgs {
     data: Array<Array<[number, number]>>
 }
 
+/**
+ * Build D3 scales from data
+ */
 const buildScales = (args: PythonArgs) => {
     const {svgWidth, svgHeight, margin, data}: PythonArgs = args
 
@@ -37,18 +46,16 @@ const D3Component = (props: ComponentProps) => {
     const {svgWidth, svgHeight, margin, data}: PythonArgs = props.args
     const transitionMillisec = 1200
 
-    // on mount, create containers for circles, line and axis
+    // On mount, create group containers for circles, path and both axis
     useEffect(() => {
         const svgElement = d3.select(svgRef.current)
         svgElement.append("g").classed('circles', true)
         svgElement.append("g").classed('line', true)
-        svgElement.append("g")
-            .classed('x-axis', true)
-        svgElement.append("g")
-            .classed('y-axis', true)
+        svgElement.append("g").classed('x-axis', true)
+        svgElement.append("g").classed('y-axis', true)
     }, [])
 
-    // create / update axis
+    // Hook to create / update axis
     useEffect(() => {
         const svgElement = d3.select(svgRef.current)
         const [xScale, yScale] = buildScales(props.args)
@@ -64,7 +71,7 @@ const D3Component = (props: ComponentProps) => {
         svgElement.select(".y-axis").call(yAxis);
     })
 
-    // create / update circles
+    // Hook to create / update circles
     useEffect(() => {
         const svgElement = d3.select(svgRef.current)
         const [xScale, yScale] = buildScales(props.args)
@@ -74,15 +81,15 @@ const D3Component = (props: ComponentProps) => {
             .join(
                 enter => (
                     enter.append("circle")
+                        // Bind each circle to [x,y] coordinate
                         .classed(styles.circle, true)
                         .attr("cx", (d: any) => xScale(d[0]))
                         .attr("cy", (d: any) => yScale(d[1]))
                         .attr("fill", "cornflowerblue")
                         .attr("r", 0)
-                        .call(el =>
-                            el.transition().duration(transitionMillisec)
-                                .attr("r", 15)
-                        )
+                        // Transition from invisible to visible circle
+                        .call(el => el.transition().duration(transitionMillisec).attr("r", 15))
+                        // Add d3 mouseover to display and move tooltip around
                         .on("mouseover", (d: any, i, ns) => {
                             const [x, y] = d3.mouse(ns[i])
                             d3.select(".tooltip")
@@ -96,13 +103,21 @@ const D3Component = (props: ComponentProps) => {
                         })
                 ),
                 update => update.call(el =>
+                    // If circle has not changed coordinates, maybe data scale changed
+                    // so transition from original position to new position
                     el.transition().duration(transitionMillisec)
                         .attr("cy", (d: any) => yScale(d[1]))
+                        // NB : keep radius value, it seems in Streamlit lifecycle there are 2 renders when mounting ?
+                        // so circles enter and during transition to full radius rerender
+                        // so if r < 15 while update then animation breaks and circle stay small for first render
                         .attr("r", 15)
                 ),
                 exit => (
-                    exit.on("mouseover", null)
+                    // Close tooltip and remove mouse events
+                    exit.dispatch("mouseout")
+                        .on("mouseover", null)
                         .on("mouseout", null)
+                        // Transition from visible to invisible circle then remove entirely
                         .call(el =>
                             el.transition().duration(transitionMillisec / 2)
                                 .attr("r", 0)
@@ -124,21 +139,20 @@ const D3Component = (props: ComponentProps) => {
             .y(d => yScale(d[1]))
 
         svgElement.select(".line").selectAll("path")
-            .data([data]) // array with 1 element it's keyed by index, enters once then updates
+            .data([data]) // array with 1 element keyed by index, so enters once then only updates
             .join(
                 enter => enter.append("path")
                     .attr("d", (d: any) => line(d))
                     .attr("stroke", "black")
                     .attr("fill", "none")
                     .attr("opacity", 0)
-                    .call(el => el.transition().duration(transitionMillisec)
-                        .attr("opacity", 1)
-                    ),
+                    .call(el => el.transition().duration(transitionMillisec).attr("opacity", 1)),
                 update => update
                     .attr("opacity", 0.2)
                     .call(el =>
                         el.transition().duration(transitionMillisec)
-                            .attr("d", (d: any) => line(d)).attr("opacity", 1)
+                            .attr("d", (d: any) => line(d))
+                            .attr("opacity", 1)
                     ),
             )
     })
